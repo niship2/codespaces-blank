2e7b0121a5ffde508b06dbda7b55c041920ea9b7
niship2 <48500095+niship2@users.noreply.github.com>
2023-03-14 07:14:28 +0000

    add mapdf

--- pages/課題解決手段データ加工.py
+++ pages/課題解決手段データ加工.py
@@ -5,21 +5,26 @@ from streamlit_app import check_password
 
 
 if check_password():
-    uploaded_file = st.file_uploader("Choose a file")
-    if uploaded_file is not None:
-        # To read file as bytes:
-        # bytes_data = uploaded_file.getvalue()
-        # st.write(bytes_data)
+    with st.expander("ファイルアップロード"):
+        uploaded_file = st.file_uploader("Choose a file")
+        if uploaded_file is not None:
+            # To read file as bytes:
+            # bytes_data = uploaded_file.getvalue()
+            # st.write(bytes_data)
 
-        # To convert to a string based IO:
-        # stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        # st.write(stringio)
+            # To convert to a string based IO:
+            # stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+            # st.write(stringio)
 
-        # To read file as string:
-        # string_data = stringio.read()
-        # st.write(string_data)
+            # To read file as string:
+            # string_data = stringio.read()
+            # st.write(string_data)
 
-        # Can be used wherever a "file-like" object is accepted:
-        dataframe = pd.read_csv(uploaded_file, encoding="cp932")
-        edited_df = st.experimental_data_editor(dataframe)
-        # st.write(dataframe)
+            # Can be used wherever a "file-like" object is accepted:
+            dataframe = pd.read_csv(uploaded_file, encoding="cp932")
+            edited_df = st.experimental_data_editor(dataframe)
+            # st.write(dataframe)
+
+            st.write("データ加工")
+            init_df = pd.DataFrame()
+            map_df = st.experimental_data_editor(init_df)
