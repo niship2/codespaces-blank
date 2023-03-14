9542a3e7ee4cb231d07eb386383e5481378492f5
niship2 <48500095+niship2@users.noreply.github.com>
2023-03-14 07:39:53 +0000

    fix:init dataframe

--- pages/課題解決手段データ加工.py
+++ pages/課題解決手段データ加工.py
@@ -5,7 +5,7 @@ from streamlit_app import check_password
 
 
 if check_password():
-    with st.expander("ファイルアップロード"):
+    with st.expander("ファイルアップロード&指定"):
         uploaded_file = st.file_uploader("Choose a file")
         if uploaded_file is not None:
             # To read file as bytes:
@@ -19,15 +19,18 @@ if check_password():
             # To read file as string:
             # string_data = stringio.read()
             # st.write(string_data)
+    with st.expander("param指定"):
+        # Can be used wherever a "file-like" object is accepted:
+        dataframe = pd.read_csv(uploaded_file, encoding="cp932")
+        edited_df = st.experimental_data_editor(dataframe)
+        # st.write(dataframe)
 
-            # Can be used wherever a "file-like" object is accepted:
-            dataframe = pd.read_csv(uploaded_file, encoding="cp932")
-            edited_df = st.experimental_data_editor(dataframe)
-            # st.write(dataframe)
+    with st.expander("データ加工")
+        num_rows = st.slider('行数指定', 2, 25, 2)
+        num_cols = st.slider('列数指定', 2, 25, 2)
 
-            st.write("データ加工")
-            init_df = pd.DataFrame([[""], [""]])
-            map_df = st.experimental_data_editor(init_df, num_rows="dynamic")
+        init_df = pd.DataFrame(np.arange(num_rows * num_cols).reshape(num_rows, num_cols))
+        map_df = st.experimental_data_editor(init_df, num_rows="dynamic")
 
-            kadai_dic = map_df.to_dict(orient="records")
-            st.write(kadai_dic)
+        kadai_dic = map_df.to_dict(orient="records")
+        st.write(kadai_dic)
